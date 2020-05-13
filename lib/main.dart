6eb4a79ff75d6d211573fc1f505aa244dfd94972
David Borrego <borregodavid@hotmail.com>
2020-05-13 18:25:24 +0200

    MiCard

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.red,
+              radius: 70.0,
+              backgroundImage: AssetImage('images/david.jpg'),
+            ),
+            Text(
+              'David Borrego',
+              style: TextStyle(
+                  fontSize: 35, 
+                  color: Colors.white, 
+                  fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 60.0,
+              margin: EdgeInsets.symmetric(horizontal: 30.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              padding: EdgeInsetsDirectional.only(start: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 40.0,
+                  ),
+                  Text(
+                    '+34 651 375 300',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              width: double.infinity,
+              height: 60.0,
+              margin: EdgeInsets.symmetric(horizontal: 30.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              padding: EdgeInsetsDirectional.only(start: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 40.0,
+                  ),
+                  Text(
+                    'davidborregoa@hotmail.com',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
